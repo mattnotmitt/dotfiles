e4934a1839c958a719c5ea002b95fd0f9295fdef
Matt <matt@artemisbot.uk>
2019-02-18 11:01:26 +0000

    Added polybar config

---
+++ polybar/.config/polybar/mpris.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Specifying the icon(s) in the script
+# This allows us to change its appearance conditionally
+icon=""
+
+player_status=$(playerctl status 2> /dev/null)
+if [[ $? -eq 0 ]]; then
+    metadata="$(playerctl metadata title | awk '{if (length($0) > 20) {print substr($0, 0, 21)"…"} else print $0}') - $(playerctl metadata artist | awk '{if (length($0) > 20) {print substr($0, 0, 21)"…"} else print $0}')"
+fi
+
+# Foreground color formatting tags are optional
+if [[ $player_status = "Playing" ]]; then
+    echo " $metadata  "       # Orange when playing
+elif [[ $player_status = "Paused" ]]; then
+    echo " $metadata  "
+else
+    echo "​"                 # Greyed out icon when stopped
+fi
